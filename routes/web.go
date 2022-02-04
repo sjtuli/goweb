820625a5530433091419c8d429bcd53f0623e90d
sjtuli <lyha23@sjtu.edu.cn>
2022-02-04 23:18:57 +0800

    登录表单和会话控制

--- routes/web.go
+++ routes/web.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	controllers "goblog/app/http/controller"
+	"goblog/app/http/middlewares"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -31,10 +32,16 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.HandleFunc("/auth/register", auc.Register).Methods("GET").Name("auth.register")
 	r.HandleFunc("/auth/do-register", auc.DoRegister).Methods("POST").Name("auth.doregister")
 
+	r.HandleFunc("/auth/login", auc.Login).Methods("GET").Name("auth.login")
+	r.HandleFunc("/auth/dologin", auc.DoLogin).Methods("POST").Name("auth.dologin")
+
 	// 静态资源
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
 	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
 
 	// 中间件：强制内容类型为 HTML
 	//r.Use(middlewares.ForceHTML)
+
+	// 开始会话
+	r.Use(middlewares.StartSession)
 }
